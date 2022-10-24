9dccd4e7a2ebe53a62c1f536030bc61659a3eb41
SatriaPradana09 <2018050@scholar.itn.ac.id>
2022-10-24 21:28:24 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas1_mobile_profile/profil.java
@@ -0,0 +1,14 @@
+package com.example.tugas1_mobile_profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
